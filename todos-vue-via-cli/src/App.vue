84c4bbb0af9c9178ed9e1f53149ab12a760614f4
liudonghua123 <liudonghua123@gmail.com>
2019-05-14 14:31:40 +0800

    rewrite using vue-class-component

--- todos-vue-via-cli/src/App.vue
+++ todos-vue-via-cli/src/App.vue
@@ -48,8 +48,11 @@
 </template>
 
 <script>
-export default {
-  name: "App",
+import Vue from "vue";
+import Component from "vue-class-component";
+
+@Component
+export default class extends Vue {
   data() {
     return {
       todoItems: [],
@@ -66,179 +69,179 @@ export default {
         }
       }
     };
-  },
+  }
 
   /**
-   * 当组件挂载时，类似于 ready 事件
+   * 组件创建
    */
   async created() {
     await this.login();
     await this.loadTodoItems();
-  },
+  }
 
-  methods: {
-    /**
-     * 登录获取其他api接口的认证 token
-     */
-    async login() {
-      const loginData = {
-        email: "test@test.com",
-        password: "test"
-      };
-      try {
-        // 获取 token
-        let response = await fetch(`${this.api_host}/api/users/login`, {
-          ...this.commonFetchOptions,
-          method: "POST",
-          body: JSON.stringify(loginData)
-        });
-        const { token } = await response.json();
-        // 设置通用的ajax请求参数中请求头里面的Authorization
-        this.commonFetchOptions.headers["Authorization"] = token;
-      } catch (e) {
-        console.log(`Woops!, some error ${e.message} occurs`);
-      }
-    },
+  /**
+   * 组件加载，类似于 react 的 componentDidMount
+   */
+  mounted() {
+    // console.info(`mounted`);
+  }
 
-    /**
-     * 获取 todoItems 数据
-     */
-    async loadTodoItems() {
-      try {
-        let response = await fetch(
-          `${this.api_host}/api/todos/`,
-          this.commonFetchOptions
-        );
-        response = await response.json();
-        const todoItems = response.data;
-        console.info(`initial todoItems: ${JSON.stringify(todoItems)}`);
-        this.todoItems = todoItems;
-        this.originTodoItems = todoItems;
-      } catch (e) {
-        console.log(`Woops!, some error ${e.message} occurs`);
-      }
-    },
-
-    /**
-     * 添加 todoItem
-     */
-    async addTodoItem(todoItem) {
-      try {
-        let response = await fetch(`${this.api_host}/api/todos/`, {
-          ...this.commonFetchOptions,
-          method: "POST",
-          body: JSON.stringify(todoItem)
-        });
-        response = await response.json();
-        console.info(`added todoItem: ${JSON.stringify(response.data)}`);
-        await this.loadTodoItems();
-      } catch (e) {
-        console.log(`Woops!, some error ${e.message} occurs`);
-      }
-    },
-
-    /**
-     * 更新 todoItem
-     */
-    async updateTodoItem(todoItem) {
-      try {
-        let response = await fetch(
-          `${this.api_host}/api/todos/${todoItem._id}`,
-          {
-            ...this.commonFetchOptions,
-            method: "PUT",
-            body: JSON.stringify(todoItem)
-          }
-        );
-        response = await response.json();
-        console.info(`updated todoItem: ${JSON.stringify(response.data)}`);
-        await this.loadTodoItems();
-      } catch (e) {
-        console.log(`Woops!, some error ${e.message} occurs`);
-      }
-    },
-
-    /**
-     * 删除已完成的 todoItem 数据
-     */
-    async deleteCompletedTodoItems() {
-      try {
-        const deleteIds = this.todoItems
-          .filter(todoItem => todoItem.isCompleted)
-          .map(todoItem => todoItem._id);
-        let response = await fetch(`${this.api_host}/api/todos/`, {
-          ...this.commonFetchOptions,
-          method: "DELETE",
-          body: JSON.stringify({
-            _ids: deleteIds
-          })
-        });
-        response = await response.json();
-        console.info(`deleted todoItem: ${JSON.stringify(response.data)}`);
-        await this.loadTodoItems();
-      } catch (e) {
-        console.log(`Woops!, some error ${e.message} occurs`);
-      }
-    },
-
-    /**
-     * 输入框点击Enter键，创建一个todo
-     */
-    onCreateItem(e) {
-      if (this.todoText.trim()) {
-        const todoItem = {
-          text: this.todoText,
-          isCompleted: false
-        };
-        this.todoText = "";
-        this.addTodoItem(todoItem);
-      }
-    },
-
-    /**
-     * 改变 todo 的 isCompleted 状态的回调
-     */
-    async toggleCompleted(id) {
-      let todoItem = this.todoItems.find(item => item._id == id);
-      todoItem.isCompleted = !todoItem.isCompleted;
-      this.updateTodoItem(todoItem);
-    },
-
-    /**
-     * 过滤所有的 todoItems
-     */
-    filterAll() {
-      this.todoItems = this.originTodoItems;
-    },
-
-    /**
-     * 过滤活动的 todoItems
-     */
-    filterActive() {
-      this.todoItems = this.originTodoItems.filter(
-        item => item.isCompleted == false
-      );
-    },
-
-    /**
-     * 过滤已经完成的 todoItems
-     */
-    filterCompleted() {
-      this.todoItems = this.originTodoItems.filter(
-        item => item.isCompleted == true
-      );
-    },
-
-    /**
-     * 删除已经完成的 todoItems
-     */
-    clearCompleted() {
-      this.deleteCompletedTodoItems();
+  /**
+   * 登录获取其他api接口的认证 token
+   */
+  async login() {
+    const loginData = {
+      email: "test@test.com",
+      password: "test"
+    };
+    try {
+      // 获取 token
+      let response = await fetch(`${this.api_host}/api/users/login`, {
+        ...this.commonFetchOptions,
+        method: "POST",
+        body: JSON.stringify(loginData)
+      });
+      const { token } = await response.json();
+      // 设置通用的ajax请求参数中请求头里面的Authorization
+      this.commonFetchOptions.headers["Authorization"] = token;
+    } catch (e) {
+      console.log(`Woops!, some error ${e.message} occurs`);
     }
-  },
-  computed: {},
-  watch: {}
-};
+  }
+
+  /**
+   * 获取 todoItems 数据
+   */
+  async loadTodoItems() {
+    try {
+      let response = await fetch(
+        `${this.api_host}/api/todos/`,
+        this.commonFetchOptions
+      );
+      response = await response.json();
+      const todoItems = response.data;
+      console.info(`initial todoItems: ${JSON.stringify(todoItems)}`);
+      this.todoItems = todoItems;
+      this.originTodoItems = todoItems;
+    } catch (e) {
+      console.log(`Woops!, some error ${e.message} occurs`);
+    }
+  }
+
+  /**
+   * 添加 todoItem
+   */
+  async addTodoItem(todoItem) {
+    try {
+      let response = await fetch(`${this.api_host}/api/todos/`, {
+        ...this.commonFetchOptions,
+        method: "POST",
+        body: JSON.stringify(todoItem)
+      });
+      response = await response.json();
+      console.info(`added todoItem: ${JSON.stringify(response.data)}`);
+      await this.loadTodoItems();
+    } catch (e) {
+      console.log(`Woops!, some error ${e.message} occurs`);
+    }
+  }
+
+  /**
+   * 更新 todoItem
+   */
+  async updateTodoItem(todoItem) {
+    try {
+      let response = await fetch(`${this.api_host}/api/todos/${todoItem._id}`, {
+        ...this.commonFetchOptions,
+        method: "PUT",
+        body: JSON.stringify(todoItem)
+      });
+      response = await response.json();
+      console.info(`updated todoItem: ${JSON.stringify(response.data)}`);
+      await this.loadTodoItems();
+    } catch (e) {
+      console.log(`Woops!, some error ${e.message} occurs`);
+    }
+  }
+
+  /**
+   * 删除已完成的 todoItem 数据
+   */
+  async deleteCompletedTodoItems() {
+    try {
+      const deleteIds = this.todoItems
+        .filter(todoItem => todoItem.isCompleted)
+        .map(todoItem => todoItem._id);
+      let response = await fetch(`${this.api_host}/api/todos/`, {
+        ...this.commonFetchOptions,
+        method: "DELETE",
+        body: JSON.stringify({
+          _ids: deleteIds
+        })
+      });
+      response = await response.json();
+      console.info(`deleted todoItem: ${JSON.stringify(response.data)}`);
+      await this.loadTodoItems();
+    } catch (e) {
+      console.log(`Woops!, some error ${e.message} occurs`);
+    }
+  }
+
+  /**
+   * 输入框点击Enter键，创建一个todo
+   */
+  onCreateItem() {
+    if (this.todoText.trim()) {
+      const todoItem = {
+        text: this.todoText,
+        isCompleted: false
+      };
+      this.todoText = "";
+      this.addTodoItem(todoItem);
+    }
+  }
+
+  /**
+   * 改变 todo 的 isCompleted 状态的回调
+   */
+  async toggleCompleted(id) {
+    let todoItem = this.todoItems.find(item => item._id == id);
+    todoItem.isCompleted = !todoItem.isCompleted;
+    this.updateTodoItem(todoItem);
+  }
+
+  /**
+   * 过滤所有的 todoItems
+   */
+  filterAll() {
+    this.todoItems = this.originTodoItems;
+  }
+
+  /**
+   * 过滤活动的 todoItems
+   */
+  filterActive() {
+    this.todoItems = this.originTodoItems.filter(
+      item => item.isCompleted == false
+    );
+  }
+
+  /**
+   * 过滤已经完成的 todoItems
+   */
+  filterCompleted() {
+    this.todoItems = this.originTodoItems.filter(
+      item => item.isCompleted == true
+    );
+  }
+
+  /**
+   * 删除已经完成的 todoItems
+   */
+  clearCompleted() {
+    this.deleteCompletedTodoItems();
+  }
+}
 </script>
 
 <style>
